2d34d26c70a394b49f167e435d054ec47f8f5f45
Manuel Bogataj <manuel.bogataj@gmail.com>
2020-09-13 19:10:50 +0200

    EvadePredatorState
    NOT TESTED, maybe it needs to be solved differently

--- Assets/Scripts/AI/StateMachineEntity.cs
+++ Assets/Scripts/AI/StateMachineEntity.cs
@@ -14,13 +14,15 @@ public class StateMachineEntity : MonoBehaviour
 
     public float moveSpeed = 2f;
 
-    public float viewRadius = 10f;
+    public static float viewRadius = 10f;
 
     public static List<GameObject> plantsInRange = new List<GameObject>(); //Static weil ich sonst nicht im SearchFoodState unter Update drauf zugreifen kann
+    public static List<GameObject> predatorInRange = new List<GameObject>();
 
 
 
-
+    //Need to detect water tiles, they have a noise float below 0.4 (see inspector -> MapGenerator)
+    //Get an array/list? of all water tiles in view Distance, go to nearest one
     public class SearchWaterState : State<StateMachineEntity>
     {
         public override void Update()
@@ -39,7 +41,7 @@ public class StateMachineEntity : MonoBehaviour
             GameObject bestPlant = null; //bestPlant = closest plant
             foreach (var plant in plantsInRange)
             {
-                float dist = Vector2.Distance(plant.transform.position, objectReference.transform.position);
+                float dist = Vector3.Distance(plant.transform.position, objectReference.transform.position);
                 if (dist < bestDistance)
                 {
                     bestDistance = dist;
@@ -55,9 +57,42 @@ public class StateMachineEntity : MonoBehaviour
         }
     }
 
+    //Vielleicht auch ander lösbar https://answers.unity.com/questions/868003/navmesh-flee-ai-flee-from-player.html
+    //Muss so gemacht werden das predator aufjedenfall das tier einholt vom moveSpeed her
     public class EvadePredatorState : State<StateMachineEntity>
     {
+        //Find closest predator in range, run away from him
 
+        public override void Update()
+        {
+            float bestDistance = Mathf.Infinity;
+            GameObject bestPredator = null; //to prioritze the predator who is the nearest
+            foreach (var predator in predatorInRange) //to find out which predator is the nearest
+            {
+                float dist = Vector3.Distance(objectReference.transform.position, predator.transform.position);
+                if (dist < bestDistance)
+                {
+                    bestDistance = dist;
+                    bestPredator = predator;
+                }
+            }
+
+            if (bestPredator != null) //wenn er den nähesten gefunden hat
+            {
+                float distance = Vector3.Distance(objectReference.transform.position, bestPredator.transform.position); //distance between predator and animal
+                Debug.Log("Distance: " + distance);
+
+                if (distance < viewRadius * 2) //*2 weil radius 100? von mitte aus geht in eine richtung 50 und in die andere nochmal 50?
+                {
+                    Vector3 dirToPredator = objectReference.transform.position - bestPredator.transform.position;
+
+                    Vector3 newPos = objectReference.transform.position + dirToPredator;
+
+                    objectReference.agent.SetDestination(newPos);
+                }
+
+            }
+        }
     }
 
     public class IdleState : State<StateMachineEntity> //Animal/rabbit should wander around searching for water/plant, as it can only detect water/plant in its view Radius
@@ -118,6 +153,10 @@ public class StateMachineEntity : MonoBehaviour
             if (hit.gameObject.tag == "Plant")
             {
                 AddObjectToListOnce(hit.gameObject, plantsInRange);
+            }
+            if (hit.gameObject.tag == "Predator")
+            {
+                AddObjectToListOnce(hit.gameObject, predatorInRange);
             }
         }
         Debug.Log(plantsInRange.Count);
