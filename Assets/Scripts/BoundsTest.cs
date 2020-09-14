e2001d002afe7e7deab55dd0ca6b1270934b3cad
Manuel Bogataj <manuel.bogataj@gmail.com>
2020-09-14 14:54:24 +0200

    fixed spawning on mesh, trees/plants now spawn on mesh where no water is located

---
+++ Assets/Scripts/BoundsTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoundsTest : MonoBehaviour
+{
+    public Renderer r;
+    public GameObject tree;
+    public GameObject plant;
+
+    float randomX;
+    float randomZ;
+
+    public int numberOfTrees;
+    public int currentTrees;
+
+    public MapGenerator mapGen;
+
+    private void Awake()
+    {
+        mapGen = FindObjectOfType<MapGenerator>();
+    }
+
+    private void Start()
+    {
+        r = GetComponent<Renderer>();
+
+
+
+    }
+
+    private void Update()
+    {
+        //SpawnTrees();
+    }
+
+    public void SpawnTrees(float x, float y)
+    {
+        Debug.Log("XMin: " + r.bounds.min.x + " XMax: " + r.bounds.max.x);
+        Debug.Log("ZMin: " + r.bounds.min.z + " ZMax: " + r.bounds.max.z);
+
+        //randomX = Random.Range(r.bounds.min.x, r.bounds.max.x);
+        //randomZ = Random.Range(r.bounds.min.z, r.bounds.max.z);
+
+        RaycastHit hit;
+        //if (currentTrees <= numberOfTrees)
+        //{
+            Debug.DrawRay(new Vector3(x * 10f, 100f, y * 10f), Vector3.down * Mathf.Infinity, Color.red, 30f);
+            if (Physics.Raycast(new Vector3(-(x * 10f - 315f), 100f, -(y * 10f - 315f)), Vector3.down, out hit, Mathf.Infinity)/* && Random.value <= 0.5f*/)
+            {
+                Instantiate(tree, hit.point + new Vector3(0, 5f, 0), Quaternion.identity);
+                Debug.Log("Hit Point: " + hit.point);
+                //currentTrees += 1;
+            }
+            else
+            {
+                Debug.Log("No Ray!");
+                return;
+            }
+        //}
+    }
+
+    public void SpawnPlants(float x, float y)
+    {
+        RaycastHit hit;
+        Debug.DrawRay(new Vector3(x * 10f, 100f, y * 10f), Vector3.down * Mathf.Infinity, Color.red, 30f);
+        if (Physics.Raycast(new Vector3(-(x * 10f - 315f), 100f, -(y * 10f - 315f)), Vector3.down, out hit, Mathf.Infinity)/* && Random.value <= 0.5f*/)
+        {
+            Instantiate(plant, hit.point + new Vector3(0, 5f, 0), Quaternion.identity);
+            Debug.Log("Hit Point: " + hit.point);
+        }
+        else
+        {
+            Debug.Log("No Ray!");
+            return;
+        }
+
+    }
+}
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -15,6 +15,7 @@ public class MapGenerator : MonoBehaviour
 
     [SerializeField]
     private int mapChunkSize = 241; // Sebastian Lague E:06 LOD
+    public int Chunksize { get { return mapChunkSize; } }
 
     [Range (0,6)]
     public int levelOfDetail; //increment value, 1 if no simplification, otherwise 2,4,8,12 for increasing levels of simplification
@@ -41,6 +42,15 @@ public class MapGenerator : MonoBehaviour
 
     public GameObject plant;
 
+    public BoundsTest boundsTest;
+
+    float randomX;
+    float randomZ;
+    public int numberOfTrees;
+    int currentTrees;
+
+    public GameObject tree;
+
     public void GenerateMap()
     {
         //fetching the 2D noise map from the noise class; later on much more stuff to process the noise map to turn it into terrain map
@@ -55,6 +65,22 @@ public class MapGenerator : MonoBehaviour
                 float currentHeight = noiseMap[x, y];
                 for (int i = 0; i < regions.Length; i++)
                 {
+
+                    if (regions[i].height >= 0.8f)
+                    {
+                        if (Random.value <= 0.025f)
+                        {
+                            //Debug.Log(x + "    " + y);
+                            //if (Random.Range(0,101) < 20)
+                            //{
+                            Debug.Log(x + "    " + y);
+                            boundsTest.SpawnTrees(x, y);
+                            //}
+                        }
+
+
+                    }
+
                     if (currentHeight <= regions[i].height)//we found the region that it falls within
                     {
                         colorMap[y * mapChunkSize + x] = regions[i].color; //now all colors are saved in the array
@@ -62,16 +88,53 @@ public class MapGenerator : MonoBehaviour
                         //Spawning Plants on grass only
                         //TODO: Fix the alignment, blocks should be set left and top half the amount, see ingame
                         //FIXED: line above, but only for 64 chunksize
-                        if (regions[i].height >= 0.8f)
-                        {
-                            if (Random.value <= 0.025f)
-                            {
-                                //Instantiate(plant, new Vector3(-(x * 10 - 315), 2, -(y * 10 - 315)), Quaternion.identity);
-                            }
-                        }
+
+                        //boundsTest.SpawnTrees(regions[i].height);
+
+                        //if (regions[i].height >= 0.8f)
+                        //{
+                        //    if (Random.value <= 0.5f)
+                        //    {
+                        //        if (currentTrees <= numberOfTrees)
+                        //        {
+                        //            RaycastHit hit;
+                        //            if (Physics.Raycast(new Vector3(x, y), -Vector3.up, out hit))
+                        //            {
+                        //                Instantiate(tree, hit.point + new Vector3(0, 5f, 0), Quaternion.identity);
+                        //                currentTrees += 1;
+                        //            }
+                        //        }
+
+
+                        //    }
+                        //}
+
+
+
+
+                        //if (regions[i].height >= 0.8f)
+                        //{
+                        //    if (Random.value <= 0.025f)
+                        //    {
+                        //        Instantiate(plant, new Vector3(-(x * 10 - 315), 2, -(y * 10 - 315)), Quaternion.identity);
+                        //    }
+                        //}
+
+
+
+                        //if (regions[i].height >= 0.8f)
+                        //{
+
+                        //}
 
                         break;
                     }
+
+                    //float chunkX = Random.Range((float)x / (float)mapChunkSize, (float)(x + 1) / (float)mapChunkSize);
+                    //float chunkY = Random.Range((float)y / (float)mapChunkSize, (float)(y + 1) / (float)mapChunkSize);
+
+
+
                 }
             }
         }
@@ -114,4 +177,10 @@ public struct TerrainType
     public float height;
     public Color color;
 
+}
+
+public class Biome
+{
+    public string name;
+    public Color color;
 }
